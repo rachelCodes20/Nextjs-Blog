f31b043155f233122be7ca484d3ae18ed94bfb12
Rachel Collins <spiritedndiligent@gmail.com>
2023-02-27 23:42:06 -0500

    starting nav compo

---
+++ tech-tales/nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+
+function NavBar() {
+  return (
+    <nav>
+        <ul>
+            <li>
+            
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
