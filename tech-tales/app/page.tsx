6e96bfdda4cd4785f0bec01c8db05b798c258ebb
Rachel Collins <spiritedndiligent@gmail.com>
2023-03-02 23:59:39 -0500

    clean up

--- tech-tales/app/page.tsx
+++ tech-tales/app/page.tsx
@@ -74,13 +74,6 @@ export default function Home() {
           </h2>
           <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
-{/* 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        > */}
           <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
           </h2>
