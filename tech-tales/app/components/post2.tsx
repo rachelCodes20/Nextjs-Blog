967ab49d86ca3fbde784b12da5d6c09da11aef0a
Rachel Collins <spiritedndiligent@gmail.com>
2023-03-04 23:56:52 -0500

    post2

---
+++ tech-tales/app/components/post2.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styles from '../page.module.css'
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function BasicCard() {
+  return (
+    <Card sx={{ minWidth: 275 }} className={styles.blogCards}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          No. 1
+        </Typography>
+        <Typography variant="h5" component="div">
+          al{bull}li{bull}ance
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          a union or association formed for mutual benefit...
+          <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small"></Button>
+      </CardActions>
+    </Card>
+  );
+}
